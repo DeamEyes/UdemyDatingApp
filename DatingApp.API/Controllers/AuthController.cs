07fcc91ca989d0b1ba72d23970c91158401a917b
DeamEyes <deameyes@gmail.com>
2020-05-22 16:32:34 -0500

    End Of 11

--- DatingApp.API/Controllers/AuthController.cs
+++ DatingApp.API/Controllers/AuthController.cs
@@ -10,6 +10,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.IdentityModel.Tokens;
 using System.IdentityModel.Tokens.Jwt;
 using Microsoft.AspNetCore.Authorization;
+using AutoMapper;
 
 namespace DatingApp.API.Controllers
 {
@@ -19,10 +20,15 @@ namespace DatingApp.API.Controllers
     {
        private readonly IAuthRepository _repo;
        private readonly IConfiguration _config;
-        public AuthController(IAuthRepository repo, IConfiguration config)
+       private readonly IMapper _mapper;
+        public AuthController(
+            IAuthRepository repo,
+            IConfiguration config,
+            IMapper mapper)
         {
             _repo = repo;
             _config = config;
+            _mapper = mapper;
         }
 
         [HttpPost("register")]
@@ -71,7 +77,12 @@ namespace DatingApp.API.Controllers
             var tokenHandler = new JwtSecurityTokenHandler();
             var token  = tokenHandler.CreateToken(tokenDescriptor);
 
-            return Ok(new {token = tokenHandler.WriteToken(token)});
+            var user = _mapper.Map<UserForListDto>(userFromRepo);
+            return Ok(new
+            {
+                token = tokenHandler.WriteToken(token),
+                user
+            });
         }
     }
 }
